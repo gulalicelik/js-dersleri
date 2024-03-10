907b34c8e629bbd03bcd5a094c4587bb09f0c82e
gulalicelik <gulalicelikmi@gmail.com>
2024-03-10 11:11:13 +0300

    js 10 03 loading task

--- js-09-03/app.js
+++ js-09-03/app.js
@@ -100,9 +100,13 @@
 
 const myContainer = document.querySelector('.post-container');
 
+
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then((data) => {
+        const mySpinner = document.querySelector('.active-spinner');
+        mySpinner.classList.remove('active-spinner');
+        mySpinner.classList.add('disable-spinner');
         data.forEach((item) => {
             const div = document.createElement('div');
             div.classList.add('post-item');
